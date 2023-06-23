dc921edca60cc3ca7a92b114bfd955f602fca3a0
Miracle Okeke <mokeke185@gmail.com>
2023-06-23 19:09:19 +0100

    Initial commit

--- my-project/src/Pages/LoginForm.jsx
+++ my-project/src/Pages/LoginForm.jsx
@@ -5,6 +5,7 @@ import { useNavigate } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { loginRedux } from "../redux/userSlice";
 import { toast } from "react-hot-toast";
+require("dotenv").config();
 
 const LoginForm = () => {
   const [formData, setFormData] = useState({
