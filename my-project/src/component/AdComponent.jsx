ace3b443865b0bee8393a984d49ddf3c2a30fa48
Miracle Okeke <mokeke185@gmail.com>
2023-06-23 11:37:32 +0100

    Initial commit

--- my-project/src/component/AdComponent.jsx
+++ my-project/src/component/AdComponent.jsx
@@ -1,24 +1,13 @@
 import React, { useEffect, useState } from "react";
-import handImage from "https://images.unsplash.com/photo-1633158829585-23ba8f7c8caf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60";
 
 const AdComponent = () => {
-  const [adImage, setAdImage] = useState(handImage);
-
-  useEffect(() => {
-    const adImages = [handImage];
-    const interval = setInterval(() => {
-      const randomIndex = Math.floor(Math.random() * adImages.length);
-      setAdImage(adImages[randomIndex]);
-    }, 2000);
-
-    return () => {
-      clearInterval(interval);
-    };
-  }, []);
-
   return (
     <div className="relative">
-      <img src={adImage} alt="Ad" className="w-full h-80" />
+      <img
+        src="https://images.unsplash.com/photo-1633158829585-23ba8f7c8caf?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bG9hbnxlbnwwfHwwfHx8MA%3D%3D&auto=format&fit=crop&w=800&q=60"
+        alt="Ad"
+        className="w-full h-80"
+      />
       <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-30">
         <h2 className="text-white text-2xl font-bold">Get Your Loan</h2>
       </div>
