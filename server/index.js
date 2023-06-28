40211239f3a1a181b1e74185e23a8c9568bddb81
Miracle Okeke <mokeke185@gmail.com>
2023-06-28 15:28:46 +0100

    Initial commit

--- server/index.js
+++ server/index.js
@@ -17,7 +17,7 @@ dotenv.config();
 const app = express();
 
 app.use(cors());
-/*app.use((req, res, next) => {
+app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "https://finflow.netlify.app");
   res.setHeader(
     "Access-Control-Allow-Methods",
@@ -25,7 +25,7 @@ app.use(cors());
   );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, auth-token");
   next();
-}); */
+});
 app.use(express.json({ limit: "10mb" }));
 const PORT = 4000;
 
